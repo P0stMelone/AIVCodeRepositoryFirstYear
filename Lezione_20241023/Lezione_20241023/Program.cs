c5ca9c536d5f8f05d0eecce096cd76bd32d0c6df
P0stMelone <riccardo.melone@outlook.com>
2025-01-25 19:30:56 +0100

    aggiunte 2 lezioni

---
+++ Lezione_20241023/Lezione_20241023/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lezione_20241023 {
+    class Program {
+        static void Main(string[] args) {
+
+            //MaxAndMin();
+            //DontTryThisAtHome();
+            //WhileExample();
+            //Ex1();
+            //Ex2();
+            //Ex3While();
+            //Ex3DoWhile();
+            Ex4();
+
+            Console.ReadLine();
+
+        }
+
+        static void MaxAndMin () {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2) {
+                Console.WriteLine("Il numero maggiore è: " + n1);
+            } else if (n2 > n1) {
+                Console.WriteLine("Il numero maggiore è: " + n2);
+            } else {
+                Console.WriteLine("Ciaone :)");
+            }
+            
+        }
+
+        static void MaxThree () {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            int max = n1;
+            if (n2 > max) {
+                max = n2;
+            }
+            if (n3 > max) {
+                max = n3;
+            }
+
+            Console.WriteLine("Il numero maggiore è: " + max);
+        }
+
+        static void IsRight () {
+
+
+            int n1 = int.Parse(Console.ReadLine());
+            if (n1 % 2 == 0) {
+                Console.WriteLine("Il numero è pari");
+            } else {
+                Console.WriteLine("Il numero è dispari");
+            }
+
+        }
+
+        static void PrintNoob () {
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
+            Console.WriteLine("9");
+            Console.WriteLine("10");
+        }
+
+        static void DontTryThisAtHome() {
+            int i = 4;
+            int j = i++ + ++i;
+
+            Console.WriteLine("i =  " + i + "   j =  " + j);
+        }
+
+        static void WhileExample () {
+            int i = 1;
+            while (i <= 10) {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+
+        static void Ex1 () {
+            int n = int.Parse(Console.ReadLine());
+            if (n == 0) {
+                Console.WriteLine("Il numero che hai inserito è 0");
+                return;
+            }
+            int i = 0;
+            while (i <= n) {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+
+        static void Ex2 () {
+            Console.WriteLine("Inserisci un numero tra 0 e 2");
+            int n = int.Parse(Console.ReadLine());
+            while (n >= 0 && n <= 2) {
+                if (n == 0) {
+                    Console.WriteLine("Ciao");
+                } else if (n == 1) {
+                    Console.WriteLine("Salve");
+                } else if (n == 2) {
+                    Console.WriteLine("Buongiorno");
+                }
+                Console.WriteLine("Inserisci un numero tra 0 e 2");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Programma terminato");
+        }
+
+        static void Ex2DoWhile () {
+            int n;
+            do {
+                Console.WriteLine("Inserisci un numero tra 0 e 2");
+                n = int.Parse(Console.ReadLine());
+                if (n == 0) {
+                    Console.WriteLine("Ciao");
+                } else if (n == 1) {
+                    Console.WriteLine("Salve");
+                } else if (n == 2) {
+                    Console.WriteLine("Buongiorno");
+                }
+            } while (n >= 0 && n <= 2);
+
+            Console.WriteLine("Programma terminato");
+        }
+
+        static void Ex3While () {
+            int n = 100;
+
+            while (n >= 0) {
+                Console.WriteLine(n);
+                //n = n - 2;
+                n -= 2;
+            }
+        }
+
+        static void Ex3DoWhile () {
+            int n = 100;
+            do {
+                Console.WriteLine(n);
+                n -= 2;
+            } while (n >= 0);
+        }
+
+        static void Ex4 () {
+            int n;
+            string userInput;
+            int sum = 0;
+            do {
+                Console.WriteLine("Inserisci un numero (somma attuale: " + sum +")");
+                userInput = Console.ReadLine();
+                n = int.Parse(userInput);
+                //sum = sum + n;
+                sum += n;
+            } while (sum != 0);
+            Console.WriteLine("Terminato");
+        }
+
+    }
+}
---
+++ Lezione_20241028/Lezione_20241028/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+
+namespace Lezione_20241028 {
+    class Program {
+        static void Main(string[] args) {
+
+            //LessonIntroduction();
+            //OutExample();
+            //Esercizio3();
+            //Calculator();
+            //BimBumBam();
+            BimBumBamLoop();
+            Console.ReadLine();
+
+        }
+
+        #region Introduction
+        static void LessonIntroduction () {
+            int pippo = 2;
+            Console.WriteLine("Il valore della variabile pippo è: " + pippo);
+            SumTwo(pippo);
+            Console.WriteLine("Dopo la chiamata al metodo SumTwo, pippo è: " + pippo);
+            int paperino = pippo;
+            paperino += 2;
+            Console.WriteLine("Dopo aver aumentato di due paperino pippo vale: " + pippo);
+
+        }
+        static void SumTwo (int param1) {
+            param1 += 2; //param1 = param1 + 2;
+        }
+        #endregion
+
+
+        #region Example
+        static void OutExample () {
+            int numberOfTime;
+            int parsedNumber;
+            //parsedNumber = AskInt(out numberOfTime);
+            AskIntPro(out parsedNumber, out numberOfTime);
+            IncrementBy2(ref numberOfTime); //baro
+            Console.WriteLine("Il numero inserito dall'utente è: " + parsedNumber +
+                " e ci ha messo: " + numberOfTime + " volte prima di inserirlo correttamente");
+        }
+
+        static int AskInt (out int numberOfTime) {
+            numberOfTime = 0;
+            int number;
+            string userInput;
+            do {
+                numberOfTime++;
+                Console.WriteLine("Inserisci un numero intero");
+                userInput = Console.ReadLine();
+            } while (!int.TryParse(userInput, out number));
+            return number;
+        }
+
+        static void AskIntPro (out int parsedNumber, out int numberOfTime) {
+            numberOfTime = 0;
+            parsedNumber = 0;
+            string userInput;
+            do {
+                numberOfTime++;
+                Console.WriteLine("Inserisci un numero intero");
+                userInput = Console.ReadLine();
+            } while (!int.TryParse(userInput, out parsedNumber));
+        }
+
+        static void IncrementBy2 (ref int variable) {
+            variable += 2;
+        }
+        #endregion
+
+        static void Esercizio2 (ref int variable) {
+            variable++;
+        }
+
+        #region Esercizio3
+        static void Esercizio3 () {
+            float a = AskFloat("Inserisci a");
+            float b = AskFloat("Inserisci b");
+            int resultType = EqResult(a, b, out float result);
+            if (resultType == 0) {
+                Console.WriteLine("L'equazione non ha soluzione");
+            } else if (resultType == 1) {
+                Console.WriteLine("L'equazione è indeterminata");
+            } else {
+                Console.WriteLine("La soluzione è: " + result);
+            }
+        }
+
+        static int EqResult (float a, float b, out float result) {
+            if (a == 0) {
+                result = 0;
+                if (b == 0) {
+                    return 1; //indeterminata
+                } else {
+                    return 0; //impossibile
+                }
+            }
+            result = -b / (float)a;
+            return 2; //ha una soluzione
+        }
+
+        static float AskFloat (string message) {
+            bool firstTime = true;
+            string userInput;
+            float number;
+            do {
+                if (firstTime) {
+                    Console.WriteLine(message);
+                } else {
+                    Console.WriteLine("L'input che hai inserito non è un numero reale");
+                }
+                userInput = Console.ReadLine();
+                firstTime = false;
+            } while (!float.TryParse(userInput, out number));
+            return number;
+        }
+        #endregion
+
+
+        #region Esercizio per casa 1
+
+        static void Calculator () {
+            char op;
+            do {
+                Console.Write("Inserisci l'operatore (q per uscire)");
+                op = Console.ReadKey().KeyChar;
+                if (op == 'q' || op == 'Q') break;
+                Console.Write(Environment.NewLine);
+                if (op == '+') {
+                    PerformSum();
+                } else if (op == '-') {
+                    PerformSub();
+                } else if (op == '*') {
+                    PerformMul();
+                } else if (op == '/') {
+                    PerformDiv();
+                }
+            } while (true);
+        }
+
+        static void PerformSum () {
+            int a = AskInteger("Inserisci il primo addendo");
+            int b = AskInteger("Inserisci il secondo addendo");
+            Console.WriteLine("Il risultato della somma è: " + Sum(a, b));
+        }
+
+        static void PerformSub () {
+            int a = AskInteger("Inserisci il minuendo");
+            int b = AskInteger("Inserisci il sottraendo");
+            Console.WriteLine("Il risultato della differenza è: " + Sub(a, b));
+        }
+
+        static void PerformMul () {
+            int a = AskInteger("Inserisci il primo fattore");
+            int b = AskInteger("Inserisci il secondo fattore");
+            Console.WriteLine("Il risultato della moltiplicazione è: " + Mul(a, b));
+        }
+
+        static void PerformDiv () {
+            int a = AskInteger("Inserisci il dividendo");
+            int b = AskInteger("Inserisci il divisore");
+            while (b == 0) {
+                b = AskInteger("Mi spiace ma non si può dividere per 0. Inserisci un numero diverso da 0");
+            }
+            Console.WriteLine("Il risultato della divisione è: " + Division(a, b));
+        }
+
+        static int Sum (int a, int b) {
+            return a + b;
+        }
+
+        static int Sub (int a, int b) {
+            return a - b;
+        }
+
+        static int Mul (int a, int b) {
+            return a * b;
+        }
+
+        static int Division (int a, int b) {
+            return a / b;
+        }
+
+
+        static int AskInteger (string message) {
+            bool firstTime = true;
+            string userInput;
+            int number;
+            do {
+                if (firstTime) {
+                    Console.WriteLine(message);
+                } else {
+                    Console.WriteLine("L'input che hai inserito non è un numero reale");
+                }
+                userInput = Console.ReadLine();
+                firstTime = false;
+            } while (!int.TryParse(userInput, out number));
+            return number;
+        }
+        #endregion
+
+
+        #region Esercizio per casa 2
+
+        static void BimBumBamLoop () {
+            bool firstTime = true;
+            do {
+                if (firstTime) {
+                    Console.WriteLine("Benvenuto nel bimbumbam più bello che ci sia");
+                } else {
+                    Console.WriteLine();
+                    Console.WriteLine("Giocatori preparatevi, la nuova partita sta per cominciare");
+                }
+                firstTime = false;
+                BimBumBam();
+                Console.WriteLine();
+                Console.WriteLine("Volete fare un'altra partita? (y/n)");
+            } while (Console.ReadKey().KeyChar != 'n');
+        }
+
+        static void BimBumBam () {
+            int numberOfRound = AskInteger("Inserisci il numero di round");
+            int p1RoundWon = 0;
+            int p2RoundWon = 0;
+            for (int i = 0; i < numberOfRound; i++) {
+                bool p1Right = AskRightOrOdd();
+                Console.Write(Environment.NewLine);
+                int p1N = AskInteger("Giocatore 1 inserisci il tuo numero");
+                Console.Clear();
+                int p2N = AskInteger("Giocatore 2 inserisci il tuo numero");
+                bool isRight = (p1N + p2N) % 2 == 0;
+                if (p1Right && isRight) {
+                    p1RoundWon++;
+                } else if (!p1Right && !isRight) {
+                    p1RoundWon++;
+                } else {
+                    p2RoundWon++;
+                }
+                if (p1RoundWon > numberOfRound / 2f || 
+                    p2RoundWon > numberOfRound / 2f) {
+                    break;
+                }
+                Console.WriteLine("Risultati parziali:");
+                Console.WriteLine("Giocatore 1: " + p1RoundWon);
+                Console.WriteLine("Giocatore 2: " + p2RoundWon);
+            }
+            Console.WriteLine();
+            Console.WriteLine("PARTITA FINITA");
+            if (p1RoundWon > p2RoundWon) {
+                Console.WriteLine("Giocatore 1 vince " + p1RoundWon + " a " + p2RoundWon);
+            } else {
+                Console.WriteLine("Giocatore 2 vince " + p2RoundWon + " a " + p1RoundWon);
+            }
+        }
+
+        static bool AskRightOrOdd () {
+            Console.WriteLine("Giocatore 1, scegli pari (y/n)");
+            return Console.ReadKey().KeyChar == 'y';
+        }
+
+        #endregion
+
+    }
+}
