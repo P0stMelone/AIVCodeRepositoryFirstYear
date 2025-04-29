014423eebefba1fd4c74004e8fc83eaf3e7a6a14
P0stMelone <riccardo.melone@outlook.com>
2025-04-29 15:15:03 +0200

    aggiunta lezione del 28

---
+++ Lezione_20250428/Aiv.Fast2D.Component - Bullets/Engine/Core/SpriteRenderer.cs
@@ -0,0 +1,91 @@
+﻿using OpenTK;
+
+namespace Aiv.Fast2D.Component {
+    public class SpriteRenderer : Component, IDrawable {
+
+        private DrawLayer layer;
+        public DrawLayer Layer {
+            get { return layer; }
+            set { layer = value; }
+        }
+        private Sprite sprite;
+        public Sprite Sprite {
+            get { return sprite; }
+        }
+        private Vector2 textureOffset;
+        public Vector2 TextureOffset {
+            get { return textureOffset; }
+            set { textureOffset = value; }
+        }
+        private Texture texture;
+        public Texture Texture {
+            get { return texture; }
+            set { texture = value; }
+        }
+        private Vector2 pivot;
+        public Vector2 Pivot {
+            get { return pivot; }
+            set {
+                pivot = value;
+                sprite.pivot = new Vector2(sprite.Width * pivot.X, sprite.Height * pivot.Y);
+            }
+        }
+        public Vector2 CenterOffset {
+            get {
+                Vector2 temp = Vector2.Zero;
+                temp.X = (0.5f - pivot.X) * Width;
+                temp.Y = (0.5f - pivot.Y) * Height;
+                return temp;
+            }
+        }
+        public float Width {
+            get { return sprite.Width; }
+        }
+        public float Height {
+            get { return sprite.Height; }
+        }
+        public Vector2 TopLeft {
+            get {
+                return transform.Position - new Vector2(Width * pivot.X, Height * pivot.Y);
+            }
+        }
+        public Vector2 BottomRight {
+            get {
+                return transform.Position + new Vector2(Width * (1 - pivot.X), Height * (1 - pivot.Y));
+            }
+        }
+
+        public SpriteRenderer (GameObject gameObject, string textureName, Vector2 pivot, DrawLayer layer) : base (gameObject) {
+            texture = GfxMgr.GetTexture(textureName);
+            sprite = new Sprite(Game.PixelsToUnit(texture.Width), 
+                Game.PixelsToUnit(texture.Height));
+            Pivot = pivot;
+            textureOffset = Vector2.Zero;
+            this.layer = layer;
+            DrawMgr.AddItem(this);
+        }
+
+        public SpriteRenderer (GameObject gameObject, string textureName, Vector2 pivot, DrawLayer layer, 
+            float width, float height, Vector2 textureOffset) : base (gameObject) {
+            texture = GfxMgr.GetTexture(textureName);
+            sprite = new Sprite(Game.PixelsToUnit(width),Game.PixelsToUnit(height));
+            Pivot = pivot;
+            this.textureOffset = textureOffset;
+            this.layer = layer;
+            DrawMgr.AddItem(this);
+        }
+
+        public void SetAdditiveColor (Vector4 color) {
+            sprite.SetAdditiveTint(color);
+        }
+
+        public void Draw() {
+            sprite.position = transform.Position;
+            sprite.Rotation = transform.Rotation;
+            sprite.scale = transform.Scale;
+            sprite.DrawTexture(texture, (int)textureOffset.X, 
+                (int)textureOffset.Y, (int)Game.UnitToPixels(Width), 
+                (int)Game.UnitToPixels(Height));
+        }
+    }
+}
---
+++ Lezione_20250428/Aiv.Fast2D.Component - Bullets/Game/Scripts/Scene/PlayScene.cs
@@ -0,0 +1,41 @@
+﻿using OpenTK;
+
+namespace Aiv.Fast2D.Component {
+    public class PlayScene : Scene {
+        public PlayScene() : base("Game/Assets/") {
+        }
+
+        protected override void LoadAssets() {
+            AddTexture("Player", "player.png");
+            AddTexture("Link", "Link_Sheet.png");
+        }
+
+        public override void InitializeScene() {
+            base.InitializeScene();
+            //CreatePlayer();
+            CreateLink();
+        }
+
+        private void CreatePlayer () {
+            GameObject player = GameObject.CreateGameObject("Player", new Vector2(Game.Win.OrthoWidth * 0.5f, 
+                Game.Win.OrthoHeight * 0.5f));
+            player.AddComponent(new SpriteRenderer(player, "Player", Vector2.One * 0.5f, DrawLayer.Playground));
+        }
+        
+        private void CreateLink () {
+            GameObject link = GameObject.CreateGameObject("Link", new Vector2(Game.Win.OrthoWidth * 0.5f,
+                Game.Win.OrthoHeight * 0.5f));
+            link.AddComponent(new SpriteRenderer(link, "Link", Vector2.Zero, DrawLayer.Playground, 64, 64, Vector2.Zero));
+            Sheet linkSheet = new Sheet(GfxMgr.GetTexture("Link"), 8, 8);
+            SheetClip run = new SheetClip(linkSheet, "Run", new int[] { 0, 1, 2, 3, 4, 5, 6 }, true, 10);
+            SheetClip die = new SheetClip(linkSheet, "Die", new int[] { 18, 18, 19, 20, 21, 22, 23 }, false, 10);
+            SheetClip attack = new SheetClip(linkSheet, "Attack", new int[] { 38, 39, 40 }, false, 10, "Run");
+            SheetAnimator animator = new SheetAnimator(link, link.GetComponent<SpriteRenderer>());
+            link.AddComponent(animator);
+            animator.AddClip(run);
+            animator.AddClip(die);
+            animator.AddClip(attack);
+            link.AddComponent<LinkController>();
+        }
+    }
+}
